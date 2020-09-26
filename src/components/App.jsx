9801682eb08d414679391a7426331b36049686ae
Erik Shelton <erik.shelton328@gmail.com>
2020-09-26 13:17:18 -0600

    Created a card component that uses data from greenfield api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import ProductCard from './ProductCard.jsx'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ProductCard from './ProductCard.jsx';
 
-const App = () => (
-  <div>
-    <ProductCard></ProductCard>
-  </div>
-);
+const App = () => {
+  // state items:
+  // current product id
+  const [currId, setCurr] = useState(7);
+  // realted product ids - array
+  const [relatedIds, setRelated] = useState([]);
+
+  // create function to get related ids
+  const getRelated = () => {
+    return axios.get(`http://52.26.193.201:3000/products/${currId}/related`)
+      .then((res) => {
+        setRelated(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getRelated();
+  }, []);
+
+  return (
+    <div>
+      {relatedIds.map((id) =>
+        <ProductCard id={id} key={id} />,
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,42 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Rating from '@material-ui/lab/Rating';
 import {
-  Card, CardImg, CardBody, CardText,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
 } from 'reactstrap';
-import { Button } from 'reactstrap';
 
-class ProductCard extends React.Component {
-  constructor(props) {
-    super(props);
+const ProductCard = (props) => {
+  // state vars
+  // images
+  const [photos, setPhotos] = useState([{ thumbnail_url: '' }]);
+  // category
+  const [category, setCat] = useState('');
+  // title
+  const [title, setTitle] = useState('');
+  // style
+  const [style, setStyle] = useState('');
+  // Price
+  const [price, setPrice] = useState('');
+  // Rating
 
-    this.state = {
-      data: [],
-    };
+  const getItemInfo = () => {
+    axios.get(`http://52.26.193.201:3000/products/${props.id}`)
+      .then((res) => {
+        setCat(res.data.category);
+        setTitle(res.data.name);
+        setPrice(res.data.default_price);
+      });
+  };
+
+  const getStyleInfo = () => {
+    axios.get(`http://52.26.193.201:3000/products/${props.id}/styles`)
+      .then((res) => {
+        setPhotos(res.data.results[0].photos);
+        setStyle(res.data.results[0].name);
+      });
+  };
+
+  useEffect(() => {
+    getItemInfo();
+    getStyleInfo();
+  }, []);
+
+  if (photos[0].thumbnail_url === null) {
+    photos[0].thumbnail_url = 'https://safetyaustraliagroup.com.au/wp-content/uploads/2019/05/image-not-found.png';
   }
 
-  render() {
-    return (
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <CardImg variant="top" src="https://picsum.photos/200" />
-          <CardBody class='card-b'>
-            <CardText className='text-pc'>
-              CATEGORY
-            </CardText>
-            <CardText className='text-pc title-pc'>
-              Expanded Product Name with Extra Text
-            </CardText>
-            <CardText className='text-pc'>
-              $123
-            </CardText>
-            <CardText className='text-pc stars-pc'>
-              Rating
-            </CardText>
-          </CardBody>
-        </Card>
-        <Button color="danger">Danger!</Button>
-      </div>
-    );
-  }
-}
+  return (
+    <Card style={{ width: '17rem' }}>
+      <CardImg variant="top" src={photos[0].thumbnail_url} />
+      <CardBody className="card-b">
+        <CardText className="text-pc">
+          {category}
+        </CardText>
+        <CardText className="text-pc title-pc">
+          {title + ' ' + style}
+        </CardText>
+        <CardText className="text-pc">
+          {`$${price}`}
+        </CardText>
+        <CardText className="text-pc stars-pc">
+          <Rating name="size-medium" defaultValue={3} readOnly={true} />
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default ProductCard;
